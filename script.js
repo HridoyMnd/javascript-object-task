115e57d218dfa548f23703dbdb1a10be0aa90ed3
Hridoy Mnd <hridoymnd10@gmail.com>
2024-09-13 13:48:26 +0600

    object task 5 done

---
+++ script.js
@@ -0,0 +1,71 @@
+/*
+//object task 1
+// print the golden rod value 
+
+const colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    "golden rod": '#daa520'
+};
+
+let result = colors["golden rod"];
+console.log(result);
+*/
+/*
+//object task 2 
+//add a new property named passenger capacity and set the property value 5
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+car.passengerCapacity = 5;
+console.log(car);
+*/
+/*
+//object task 3
+//access the physic mark
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+ let access = student.physics.marks ;
+ console.log(access);
+*/
+/*
+ //object task 4 
+ //count the number of property 
+ let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+let count =Object.keys(student).length;
+console.log(count);
+*/
+
+/*
+//object task 5
+//print the keys value pairs with their type
+let myObject = {
+
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+    };
+
+    for(const mykey in myObject){
+        // console.log(mykey);
+        if(myObject.hasOwnProperty(mykey)) {
+            console.log(`key : ${mykey}, value : ${myObject[mykey]}`);
+        }
+    }
+    */
